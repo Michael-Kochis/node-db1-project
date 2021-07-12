d584574193016df3c33261e6b0037445a36c4b1c
Michael-Kochis <rhishisikk@gmail.com>
2021-07-12 10:26:39 -0500

    accounts-model initial pass

--- api/accounts/accounts-model.js
+++ api/accounts/accounts-model.js
@@ -1,21 +1,38 @@
+const db = require('../../data/db-config')
+
 const getAll = () => {
   // DO YOUR MAGIC
+  return db('accounts');
 }
 
 const getById = id => {
   // DO YOUR MAGIC
+  return db('accounts')
+    .where( {id} )
+    .first();
 }
 
 const create = account => {
   // DO YOUR MAGIC
+  return db('accounts')
+  .insert(account)
+  .then((result) => {
+    return getById(result[0]);
+  });
 }
 
 const updateById = (id, account) => {
   // DO YOUR MAGIC
+  return db('accounts')
+    .where({ id })
+    .update(account);
 }
 
 const deleteById = id => {
   // DO YOUR MAGIC
+  return db('posts')
+    .where({ id })
+    .del();
 }
 
 module.exports = {
